f8887a3fb6a7563e241c6741087ca20ad19fe13b
pooyan <pooyan.azadparvar@gmail.com>
2022-04-22 21:04:42 +0430

    Update monitoring.php

--- monitoring.php
+++ monitoring.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
+#css / going center and cahnge bg color
 <body style="background-color:powderblue;">
  <meta http-equiv="refresh" content="15">
 
 <center>
 <h1>Server performance monitoring</h1>
 <?php
-
+#php code to run shell script
 $output = shell_exec('./bash_script.sh|boxes -d simple');
 echo "<td align=centre><pre>$output</pre>";
 
