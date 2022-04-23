3c1c2cd8d9e11bf1a7a4523694fe004a36bbb5d8
pooyan <pooyan.azadparvar@gmail.com>
2022-04-23 07:18:51 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<!–– CSS background and button configuration ––>
+<!-- CSS background and button configuration -->
 <body style="background-color:powderblue;">
 <style>
 .button {
@@ -18,7 +18,8 @@
   cursor: pointer;
 }
 </style>
-<!–– button action -->
+<!-- button action -->
+
 <center>
 <h1>Click here to see monitoring dashboard</h1>
 <button type="button" class="button" onclick="window.location.href = 'http://<?php echo $_SERVER['HTTP_HOST']; ?>/monitoring.php';">Click Me!</button>
